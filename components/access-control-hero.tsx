b606801f2b2429cd74d3e7a2f1a1e72f70c00d68
deepak <deepak@worksdelight.org>
2025-06-12 15:25:38 +0530

    static-pages

--- components/access-control-hero.tsx
+++ components/access-control-hero.tsx
@@ -46,14 +46,6 @@ export default function AccessControlHero() {
               <Button size="lg" className="bg-white text-blue-600 hover:bg-blue-50 text-lg px-8 py-4">
                 Get Access Control Quote
               </Button>
-              <Button
-                size="lg"
-                variant="outline"
-                className="border-white text-white hover:bg-white hover:text-blue-600 text-lg px-8 py-4"
-              >
-                <Phone className="w-5 h-5 mr-2" />
-                Call Access Control Experts
-              </Button>
             </div>
 
             <div className="flex items-center space-x-6 text-blue-200">
