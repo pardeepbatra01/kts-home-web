17e79864915a0c181dd94c2e68ccc4b5aade254f
deepak <deepak@worksdelight.org>
2025-06-13 16:00:10 +0530

    static-pages

--- components/footer.tsx
+++ components/footer.tsx
@@ -60,7 +60,7 @@ export default function Footer() {
             <h3 className="text-lg font-semibold mb-4">Contact Us</h3>
             <ul className="space-y-2">
               <li><Link href="mailto:support@ktshome.ca" className="text-blue-100/80 hover:text-white text-sm">Email</Link></li>
-              <li><Link href="#" className="text-blue-100/80 hover:text-white text-sm">Phone</Link></li>
+              <li><Link href="tel:+18554587669" className="text-blue-100/80 hover:text-white text-sm">Phone</Link></li>
             </ul>
           </div>
          
